a7f0187f20fa4081add0494fa9c7f94d42bde7a8
therockstarind <rohitkumarrohit96@gmail.com>
2023-09-26 16:34:10 +0530

    Rm: protectedRoutes

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -44,5 +44,5 @@
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Yolo'],
+  protectedRoutes: '',
 }
